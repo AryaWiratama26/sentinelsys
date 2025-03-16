0bd4cdc410c89e89bc5afce38b60d8332b1f6ba4
Arya Wiratama <aryawiratama2401@gmail.com>
2025-03-17 06:59:44 +0700

    monitoring

--- sentinelsys/monitor.py
+++ sentinelsys/monitor.py
@@ -4,4 +4,13 @@ class Monitoring:
     def __init__(self):
         pass
         
+    def get_cpu_usage(self):
+        return psutil.cpu_percent(interval=1)
+    
+    def get_memory_usage(self):
+        ram = psutil.virtual_memory()
+        return ram.percent, ram.total / (1024 ** 3)
+    
+    def get_disk_usage(self):
+        return psutil.disk_usage('/').percent
 
