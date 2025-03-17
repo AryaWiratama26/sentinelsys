0e1654f104ac8479e3455b2387882623ac74e1fb
Arya Wiratama <aryawiratama2401@gmail.com>
2025-03-17 07:22:31 +0700

    update visualizer

--- sentinelsys/visualizer.py
+++ sentinelsys/visualizer.py
@@ -16,16 +16,15 @@ class VisualizerRun:
         self.disk_data.append(psutil.disk_usage('/').percent)
         self.timestamps.append(time.time())
         
-        if len(self.cpu_data > 50):
+        if len(self.cpu_data) > 50:
             self.cpu_data.pop(0)
             self.ram_data.pop(0)
             self.disk_data.pop(0)
             self.timestamps.pop(0)
             
     def plot_graph(self):
-
         plt.ion()
-        fig, ax = plt.subplots(3,1,figsize=(8,6))
+        fig, ax = plt.subplots(3, 1, figsize=(8, 6))
         
         while True:
             self.update_data()
@@ -33,19 +32,22 @@ class VisualizerRun:
             ax[0].cla()
             ax[0].plot(self.timestamps, self.cpu_data, label="CPU Usage (%)", color='r')
             ax[0].set_title("CPU Usage")
-            ax[0].set_ylim(0,100)
+            ax[0].set_ylim(0, 100)
             ax[0].legend()
-            
-            ax[0].cla()
-            ax[0].plot(self.timestamps, self.ram_data, label="RAM Usage (%)", color='g')
-            ax[0].set_title("RAM Usage")
-            ax[0].set_ylim(0,100)
-            ax[0].legend()
-            
-            ax[0].cla()
-            ax[0].plot(self.timestamps, self.disk_data, label="Disk Usage (%)", color='b')
-            ax[0].set_title("DISK Usage")
-            ax[0].set_ylim(0,100)
-            ax[0].legend()
-            
+
+            ax[1].cla()
+            ax[1].plot(self.timestamps, self.ram_data, label="RAM Usage (%)", color='g')
+            ax[1].set_title("RAM Usage")
+            ax[1].set_ylim(0, 100)
+            ax[1].legend()
+
+            ax[2].cla()
+            ax[2].plot(self.timestamps, self.disk_data, label="Disk Usage (%)", color='b')
+            ax[2].set_title("Disk Usage")
+            ax[2].set_ylim(0, 100)
+            ax[2].legend()
+
+            plt.tight_layout()
             plt.pause(self.interval)
+
+        plt.show(block=True)
